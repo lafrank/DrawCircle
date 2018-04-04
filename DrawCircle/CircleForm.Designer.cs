33b9eb522e211a0d3456f89a8f3b574c3a867801
LaFrank <laszlo.frank@gmail.com>
2018-04-04 20:22:49 +0200

    Add project files.

---
+++ DrawCircle/CircleForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace DrawCircle
+{
+	partial class CircleForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// CircleForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(511, 288);
+			this.Name = "CircleForm";
+			this.Text = "Form1";
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.CircleForm_Paint);
+			this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CircleForm_MouseClick);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CircleForm_MouseMove);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
---
+++ DrawCircle/CircleForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawCircle
+{
+	public partial class CircleForm : Form
+	{
+		Point circleCenter;
+		Point mousePosition;
+
+		public CircleForm()
+		{
+			InitializeComponent();
+		}
+
+		private void CircleForm_MouseClick(object sender, MouseEventArgs e)
+		{
+			circleCenter = e.Location;
+		}
+
+		private void CircleForm_MouseMove(object sender, MouseEventArgs e)
+		{
+			mousePosition = e.Location;
+			Invalidate();
+		}
+
+		private void CircleForm_Paint(object sender, PaintEventArgs e)
+		{
+			int radius = (int)Math.Sqrt(Math.Pow(mousePosition.X - circleCenter.X, 2) + Math.Pow(mousePosition.Y - circleCenter.Y, 2));
+			Graphics g = e.Graphics;
+			Rectangle r = new Rectangle(circleCenter.X - radius, circleCenter.Y - radius, 2 * radius, 2 * radius);
+			Pen p = Pens.Green;
+			g.DrawEllipse(p, r);
+		}
+	}
+}
