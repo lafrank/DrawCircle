e07aefe31df0ccb683a59c49c24449d9fbb39d95
LaFrank <laszlo.frank@gmail.com>
2018-04-06 22:11:06 +0200

    Rewritten the whole stuff

--- DrawCircle/CircleForm.Designer.cs
+++ DrawCircle/CircleForm.Designer.cs
@@ -28,23 +28,49 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.tslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslStatus});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 266);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(511, 22);
+			this.statusStrip1.TabIndex = 0;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// tslStatus
+			// 
+			this.tslStatus.Name = "tslStatus";
+			this.tslStatus.Size = new System.Drawing.Size(118, 17);
+			this.tslStatus.Text = "toolStripStatusLabel1";
 			// 
 			// CircleForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(511, 288);
+			this.Controls.Add(this.statusStrip1);
 			this.Name = "CircleForm";
 			this.Text = "Form1";
 			this.Paint += new System.Windows.Forms.PaintEventHandler(this.CircleForm_Paint);
 			this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CircleForm_MouseClick);
 			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CircleForm_MouseMove);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel tslStatus;
 	}
 }
 
--- DrawCircle/CircleForm.cs
+++ DrawCircle/CircleForm.cs
@@ -7,32 +7,140 @@ namespace DrawCircle
 	public partial class CircleForm : Form
 	{
 		Point circleCenter;
+		int radius = -1;
 		Point mousePosition;
+		int clickCount = 0;
+		CircleState state = CircleState.Centering;
 
 		public CircleForm()
 		{
 			InitializeComponent();
+			SetStatusText();
+		}
+
+		private void SetStatusText()
+		{
+			switch (state)
+			{
+				case CircleState.Init:
+					{
+						tslStatus.Text = "Please click to start operation";
+						break;
+					}
+				case CircleState.Centering:
+					{
+						tslStatus.Text = "Please click to select circle center";
+						break;
+					}
+				case CircleState.Sizing:
+					{
+						tslStatus.Text = "Set circle size by moving the mouse, and click to set actual size.";
+						break;
+					}
+				case CircleState.SetWedge:
+					{
+						tslStatus.Text = "Select desired wedge size by moving the mouse, and click to set actual size.";
+						break;
+					}
+			}
 		}
 
 		private void CircleForm_MouseClick(object sender, MouseEventArgs e)
 		{
-			circleCenter = e.Location;
+			clickCount++;
+			switch (state)
+			{
+				case CircleState.Init:
+					{
+						state = CircleState.Centering;
+						break;
+					}
+				case CircleState.Centering:
+					{
+						state = CircleState.Sizing;
+						circleCenter = e.Location;
+						break;
+					}
+				case CircleState.Sizing:
+					{
+						state = CircleState.SetWedge;
+						break;
+					}
+				case CircleState.SetWedge:
+					{
+						state = CircleState.Init;
+						break;
+					}
+			}
+			SetStatusText();
 		}
 
 		private void CircleForm_MouseMove(object sender, MouseEventArgs e)
 		{
-			mousePosition = e.Location;
-			Invalidate();
+			switch (state)
+			{
+				case CircleState.Init:
+					{
+						break;
+					}
+				case CircleState.Centering:
+					{
+						break;
+					}
+				case CircleState.Sizing:
+					{
+						mousePosition = e.Location;
+						Invalidate();
+						break;
+					}
+				case CircleState.SetWedge:
+					{
+						mousePosition = e.Location;
+						Invalidate();
+						break;
+					}
+			}
+			//mousePosition = e.Location;
 		}
 
 		private void CircleForm_Paint(object sender, PaintEventArgs e)
 		{
-			int radius = (int)Math.Sqrt(Math.Pow(mousePosition.X - circleCenter.X, 2) + Math.Pow(mousePosition.Y - circleCenter.Y, 2));
-			Graphics g = e.Graphics;
-			Rectangle r = new Rectangle(circleCenter.X - radius, circleCenter.Y - radius, 2 * radius, 2 * radius);
-			Pen p = Pens.Green;
-			g.DrawEllipse(p, r);
-			g.DrawLine(p, circleCenter, mousePosition);
+			switch (state)
+			{
+				case CircleState.Init:
+					{
+						break;
+					}
+				case CircleState.Centering:
+					{
+						break;
+					}
+				case CircleState.Sizing:
+					{
+						radius = (int)Math.Sqrt(Math.Pow(mousePosition.X - circleCenter.X, 2) + Math.Pow(mousePosition.Y - circleCenter.Y, 2));
+						if (radius > 0)
+						{
+							Graphics g = e.Graphics;
+							Rectangle r = new Rectangle(circleCenter.X - radius, circleCenter.Y - radius, 2 * radius, 2 * radius);
+							g.DrawEllipse(Pens.Green, r);
+						}
+						break;
+					}
+				case CircleState.SetWedge:
+					{
+						if (radius > 0)
+						{
+							Graphics g = e.Graphics;
+							Rectangle r = new Rectangle(circleCenter.X - radius, circleCenter.Y - radius, 2 * radius, 2 * radius);
+							g.DrawEllipse(Pens.Silver, r);
+							g.DrawLine(Pens.DarkOrange, circleCenter, mousePosition);
+						}
+						break;
+					}
+			}
+
 		}
 	}
+
+	public enum CircleState { Init, Centering, Sizing, SetWedge }
 }
